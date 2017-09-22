bc263553f47fdc7e5952778e31de035c349af8e6
Gauraang Khurana <GauraangNew@Gauraangs-MacBook-Air.local>
2017-09-22 14:13:11 -0400

    Model changed for car

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,13 @@ class Car < ApplicationRecord
   has_many :reservations, dependent: :destroy
   validates :plate, :make, :model, :rate, :style, :location, presence: true
 
-  
+  # validates :plate, uniqueness: { case_sensitive: false }
+  #
+  # validates :style,
+  #   :inclusion  => { :in => [ 'SUV', 'Sedan', 'Coupe' ],
+  #   :message    => "%{value} is not a valid car style." }
+  #
+  # validates :status,
+  #     :inclusion  => { :in => [ 'Checked Out', 'Available', 'Reserved' ],
+  #     :message    => "%{value} is not a valid car style." }
 end
