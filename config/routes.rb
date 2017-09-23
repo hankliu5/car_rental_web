3049b26dd142d8e69afe04e8c3a66acef563b4f4
hankliu5 <yul560@eng.ucsd.edu>
2017-09-23 18:45:23 -0400

    added user view/controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  match '/users',   to: 'users#index',   via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :cars
   root 'cars#index'
-  devise_for :users
+  devise_for :users, :path_prefix => 'd'
+  resources :users, :only =>[:show]
 end
