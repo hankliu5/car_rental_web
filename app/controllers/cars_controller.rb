4425e928e6c00d1232c6a5d474eb15461d39339f
Otto <yuh198@ucsd.edu>
2017-09-23 17:38:03 -0400

    update/destroy car_controller.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -22,12 +22,23 @@ class CarsController < ApplicationController
 	end
 
 	def edit
+		@car = Car.find(params[:id])
 	end
 
 	def update
+		@car = Car.find(params[:id])
+
+		if @car.update(car_params)
+			redirect_to cars_path, :notice => "Car information has been updated!"
+		else
+			render :edit
+		end
 	end
 
 	def destroy
+		@car = Car.find(params[:id])
+		@car.destroy
+		redirect_to cars_path, :notice => "Car information has been deleted!"
 	end
 
 	private
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,3 @@
 class Car < ApplicationRecord
+    validates :plate, :make, :model, :rate, :style, :location, presence: true
 end
