9118d1cc0ca8623197030471e12f1619f5b90296
Otto <yuh198@ucsd.edu>
2017-09-23 18:45:45 -0400

    add username column

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, :presence => true
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 end
