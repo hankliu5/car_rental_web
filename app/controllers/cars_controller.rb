bc42e10aa08fcb122a9b7a1a1de4ba6de0b32c76
hankliu5 <yul560@eng.ucsd.edu>
2017-09-24 01:06:04 -0400

    set up abilities of users.

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,49 +1,52 @@
 class CarsController < ApplicationController
-	before_action :authenticate_user!, except: :index
-	def index
-		@cars = Car.all
-	end
+  before_action :authenticate_user!, except: %i[index show]
+	load_and_authorize_resource
 
-	def new
-		@car = Car.new
-	end
+  def index
+    @cars = Car.all
+  end
 
-	def create
-		@car = Car.new(car_params)
-		@car.checkout = false
-		if @car.save
-			redirect_to cars_path
-		else
-			render :new
-		end
-	end
+  def new
+    @car = Car.new
+  end
 
-	def show
-		@car = Car.find(params[:id])
-	end
+  def create
+    @car = Car.new(car_params)
+    @car.checkout = false
+    if @car.save
+      redirect_to cars_path
+    else
+      render :new
+    end
+  end
 
-	def edit
-		@car = Car.find(params[:id])
-	end
+  def show
+    @car = Car.find(params[:id])
+  end
 
-	def update
-		@car = Car.find(params[:id])
+  def edit
+    @car = Car.find(params[:id])
+  end
 
-		if @car.update(car_params)
-			redirect_to cars_path, :notice => "Car information has been updated!"
-		else
-			render :edit
-		end
-	end
+  def update
+    @car = Car.find(params[:id])
 
-	def destroy
-		@car = Car.find(params[:id])
-		@car.destroy
-		redirect_to cars_path, :notice => "Car information has been deleted!"
-	end
+    if @car.update(car_params)
+      redirect_to cars_path, notice: 'Car information has been updated!'
+    else
+      render :edit
+    end
+  end
 
-	private
-	def car_params
-		params.require(:car).permit(:plate, :make, :model, :rate, :style, :location)
-	end
+  def destroy
+      @car = Car.find(params[:id])
+      @car.destroy
+      redirect_to cars_path, notice: 'Car information has been deleted!'
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:plate, :make, :model, :rate, :style, :location)
+  end
 end
