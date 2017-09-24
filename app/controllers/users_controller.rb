dc92e13211eee1d821c180c918c99c9d22dff3d3
hankliu5 <yul560@eng.ucsd.edu>
2017-09-24 17:08:38 -0400

    can create a new admin, added password and password confimation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,21 @@ class UsersController < ApplicationController
     end
   end
 
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(new_user_params)
+		@user.add_role(current_user.roles.find(1).name)
+		if @user.save
+				redirect_to users_path
+		else
+				render :new
+		end
+	end
+
+
   def show
     @user = User.find(params[:id])
   end
@@ -28,12 +43,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
 		admin_try_to_edit_admins
 
-    if @user.update(user_params)
+    if @user.update(update_user_params)
       redirect_to users_path, notice: 'User information has been updated!'
     else
       render :edit
     end
   end
+
+	def destroy
+			@user = User.find(params[:id])
+			@user.destroy
+			redirect_to users_path, notice: 'User information has been deleted!'
+	end
 
   private
 
@@ -46,7 +67,11 @@ class UsersController < ApplicationController
     redirect_to root_path if current_user && (!current_user.has_role? :admin)
   end
 
-	def user_params
+	def update_user_params
 		params.require(:user).permit(:username, :email)
+	end
+
+	def new_user_params
+		params.require(:user).permit(:username, :email, :password, :password_confirmation)
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,13 @@ class User < ApplicationRecord
   has_many :reservations
   validates :username, :presence => true
   validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validates :password,
+          # you only need presence on create
+          :presence => { :on => :create },
+          # allow_nil for length (presence will handle it on create)
+          :length   => { :minimum => 6, :allow_nil => true },
+          # and use confirmation to ensure they always match
+          :confirmation => true
+
+  validates :password_confirmation, :presence => true, :if => '!password.nil?'
 end
