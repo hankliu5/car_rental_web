db659c2aea204a3be169ac81c00cbff577fa2329
hankliu5 <yul560@eng.ucsd.edu>
2017-09-24 22:07:21 -0400

    can change password now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :check_permission
+  before_action :check_permission, except: [:edit_password, :update_password]
 
   def index
     @all_users = User.all
@@ -15,20 +15,19 @@ class UsersController < ApplicationController
     end
   end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(new_user_params)
-		@user.add_role(current_user.roles.find(1).name)
-		if @user.save
-				redirect_to users_path
-		else
-				render :new
-		end
-	end
-
+  def create
+    @user = User.new(new_user_params)
+    @user.add_role(current_user.roles.find(1).name)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
 
   def show
     @user = User.find(params[:id])
@@ -36,12 +35,12 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-		admin_try_to_edit_admins
+    admin_try_to_edit_admins
   end
 
   def update
     @user = User.find(params[:id])
-		admin_try_to_edit_admins
+    admin_try_to_edit_admins
 
     if @user.update(update_user_params)
       redirect_to users_path, notice: 'User information has been updated!'
@@ -50,30 +49,49 @@ class UsersController < ApplicationController
     end
   end
 
-	def destroy
-			@user = User.find(params[:id])
-      admin_try_to_edit_admins
-			if @user.destroy
-  			redirect_to users_path, notice: 'User information has been deleted!'
-      end
-	end
+  def edit_password
+    @user = current_user
+  end
+
+  def update_password
+    @user = current_user
+    if @user.update_with_password(update_password_params)
+      # Sign in the user by passing validation in case their password changed
+      bypass_sign_in(@user)
+      redirect_to root_path
+    else
+      render :edit_password
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to users_path, notice: 'User information has been deleted!'
+    end
+  end
 
   private
 
-	def admin_try_to_edit_admins
-		if (@user.has_role? :admin) && (current_user.has_role? :admin)
-			redirect_to users_path, alert: 'You cannot edit other admins as an admin.'
-		end
-	end
+  def admin_try_to_edit_admins
+    if (@user.has_role? :admin) && (current_user.has_role? :admin)
+      redirect_to users_path, alert: 'You cannot edit other admins as an admin.'
+    end
+  end
+
   def check_permission
     redirect_to root_path if current_user && (!current_user.has_role? :admin)
   end
 
-	def update_user_params
-		params.require(:user).permit(:username, :email)
-	end
+  def update_user_params
+    params.require(:user).permit(:username, :email)
+  end
 
-	def new_user_params
-		params.require(:user).permit(:username, :email, :password, :password_confirmation)
-	end
+  def update_password_params
+    params.require(:user).permit(:password, :password_confirmation, :current_password)
+  end
+
+  def new_user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
-  match '/users',   to: 'users#index',   via: 'get'
+  match '/users', to: 'users#index', via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :cars
   resources :reservations
 
   root 'cars#index'
-  devise_for :users, :path_prefix => 'd'
+  devise_for :users, path_prefix: 'd'
   resources :users
+
+  resource :user do
+    collection do
+      get 'edit_password'
+    end
+  end
+
+  resource :user, only: [:edit_password] do
+    collection do
+      patch 'update_password'
+    end
+  end
 end
