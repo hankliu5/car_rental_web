5cb5ee2613ae6bc5e43e85dc008ff6cd9ae06842
hankliu5 <yul560@eng.ucsd.edu>
2017-09-30 14:36:45 -0400

    added Car plate test case.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,12 +1,13 @@
 class Car < ApplicationRecord
   has_many :reservations, dependent: :destroy
-  validates :plate, presence: true, length: { is: 7}, uniqueness: true
-
+  validates :plate, presence: true, length: { is: 7 }, format: { with: /[a-zA-Z0-9]/ }, uniqueness: true
   validates_format_of :plate, :with => /\A[a-zA-Z0-9]+\z/, :message => "Only allows letters and numbers."
-
   validates :make, :model, :location, presence: true
   validates :rate, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :style, presence: true, inclusion: { in: %w(Coupe Sedan SUV), :message => "%{style} is invalid car style"}
+
+  # validates :location, presence: :true
+  # validates_format_of :location, :with => /[a-zA-Z]/, :message => "Only allows letters."
 
   # validates :plate, uniqueness: { case_sensitive: false }
   #
@@ -24,32 +25,28 @@ class Car < ApplicationRecord
   def self.search(term, category)
     term.to_s.downcase!
     if term && category
-        if category == "Status"
-            cars = [] # array
-            Car.all.each do |car|
-                case term
-                when "available"
-                    if (car.reservation_time.nil?) || (car.reservation_time.to_i < Time.now.to_i) && !car.checkout
-                        cars << car
-                    end
-                when "reserved"
-                    if ((car.reservation_time).to_i > Time.now.to_i) && !car.checkout
-                        cars << car
-                    end
-                when "checked out"
-                    if car.checkout
-                        cars << car
-                    end
-                end
+      if category == 'Status'
+        cars = [] # array
+        Car.all.each do |car|
+          case term
+          when 'available'
+            if car.reservation_time.nil? || (car.reservation_time.to_i < Time.now.to_i) && !car.checkout
+              cars << car
             end
-            return cars
-        else
-            where("#{category} LIKE ?", "%#{term}%")
+          when 'reserved'
+            if (car.reservation_time.to_i > Time.now.to_i) && !car.checkout
+              cars << car
+            end
+          when 'checked out'
+            cars << car if car.checkout
+          end
         end
+        return cars
+      else
+        where("#{category} LIKE ?", "%#{term}%")
+      end
     else
-        all
+      all
     end
   end
-
-
 end
--- test/models/car_test.rb
+++ test/models/car_test.rb
@@ -1,7 +1,49 @@
 require 'test_helper'
 
 class CarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test 'valid car' do
+		car = Car.new(plate: '6MVJ101', make: :'Toyota', model: 'Camry', rate: 30, style: 'Sedan', location: 'Raleigh', checkout: false)
+		assert car.valid?
+	end
+
+	test 'valid plate with all lowercase' do
+		car = Car.new(plate: 'hankliu', make: :'Toyota', model: 'Camry', rate: 30, style: 'Sedan', location: 'Raleigh', checkout: false)
+		assert car.valid?
+	end
+
+	test 'valid plate with all uppercase' do
+		car = Car.new(plate: 'HANKLIU', make: :'Toyota', model: 'Camry', rate: 30, style: 'Sedan', location: 'Raleigh', checkout: false)
+		assert car.valid?
+	end
+
+	test 'valid plate with uppercase and lowercase' do
+		car = Car.new(plate: 'HankLiu', make: :'Toyota', model: 'Camry', rate: 30, style: 'Sedan', location: 'Raleigh', checkout: false)
+		assert car.valid?
+	end
+
+	test 'valid plate with all numbers' do
+		car = Car.new(plate: '1234567', make: :'Toyota', model: 'Camry', rate: 30, style: 'Sedan', location: 'Raleigh', checkout: false)
+		assert car.valid?
+	end
+
+	test 'no plate number' do
+		car = Car.new(make: :'Toyota', model: 'Camry', rate: 30, style: 'Sedan', location: 'Raleigh', checkout: false)
+		assert !car.valid?
+	end
+
+	test 'plate number more than 7 digits' do
+		car = Car.new(plate: '00000000', make: :'Toyota', model: 'Camry', rate: 30, style: 'Sedan', location: 'Raleigh', checkout: false)
+		assert !car.valid?
+	end
+
+	test 'plate number less than 7 digits' do
+		car = Car.new(plate: '000000', make: :'Toyota', model: 'Camry', rate: 30, style: 'Sedan', location: 'Raleigh', checkout: false)
+		assert !car.valid?
+	end
+
+	test 'plate number contains special symbols' do
+		car = Car.new(plate: '000000*', make: :'Toyota', model: 'Camry', rate: 30, style: 'Sedan', location: 'Raleigh', checkout: false)
+		assert !car.valid?
+	end
+
 end
